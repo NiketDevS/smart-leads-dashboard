e73c3b9b2a9a7b150d93aef8bf9b82b333ecbd88
Niket Sarkar <niketsarkar@Nikets-MacBook-Air.local>
2026-05-17 12:46:59 +0530

    Implement JWT authentication system

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.routes";
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,7 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send("API running...");
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+
+import User from "../models/User";
+import generateToken from "../utils/generateToken";
+
+export const registerUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(400).json({
+        message: "User already exists",
+      });
+
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+
+      token: generateToken(user._id.toString()),
+
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(401).json({
+        message: "Invalid credentials",
+      });
+
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(401).json({
+        message: "Invalid credentials",
+      });
+
+      return;
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+
+      token: generateToken(user._id.toString()),
+
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
+};
